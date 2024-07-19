c53884909226072b3280ecba940b5e31ca404b44
clara <157494185+claratjx@users.noreply.github.com>
2024-07-19 13:15:01 +0800

    Update Home.js

--- assistive-app/src/components/Home.js
+++ assistive-app/src/components/Home.js
@@ -1,23 +1,22 @@
 import React from 'react';
-<!DOCTYPE html>
-  <html>
-  <head>
-  <style>
-  body {
-  font-family: Arial, san-serif;
-  background-colour: rgb(240,224,214);
-}
-h1{
-  color: black;
-  text-align: center; 
-}
-</style>
-  </head> 
-  </body>
-<h1>Memory Game</h1>
-<p>Name</p>
-  </body>
-  </html>
-export default function Home() {
 
+const styles = {
+  body: {
+    fontFamily: 'Arial, sans-serif',
+    backgroundColor: 'rgb(240, 224, 214)',
+    textAlign: 'center',
+  },
+  h1: {
+    color: 'black',
+  },
 };
+
+export default function Home() {
+  return (
+    <div style={styles.body}>
+      <h1 style={styles.h1}>Memory Game</h1>
+      <p>Name</p>
+    </div>
+  );
+}
+
